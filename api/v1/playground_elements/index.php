92c0583551ced5ec17c5812741f35544fe190780
Ilya Sidorchik <ilya@sidorchik.ru>
2019-12-08 23:59:28 +0300

    Get playground elements list of certain playground

--- api/v1/playground_elements/index.php
+++ api/v1/playground_elements/index.php
@@ -8,8 +8,12 @@ $link = mysqli_connect($ini[database][host], $ini[database][user], $ini[database
 mysqli_set_charset($link, 'utf8');
 
 $content = [];
+$content_result = [];
+$content_key_result = 'result';
+$content_status = 'OK';
 
 $playground_element_id = htmlspecialchars($_GET['id']);
+$input_playground_id = htmlspecialchars($_GET['playground_id']);
 
 if ($playground_element_id != '') {
     $result = mysqli_query($link, "SELECT * FROM `playground_elements` WHERE `id` = '$playground_element_id'");
@@ -18,32 +22,65 @@ if ($playground_element_id != '') {
     $element_id = $row['element_id'];
 
     if (isset($playground_id)) {
-        array_push($content, [
+        array_push($content_result, [
             'id' => $playground_element_id,
             'playground_id' => $playground_id,
             'element_id' => $element_id
         ]);
     } else {
-        array_push($content, [
+        array_push($content_result, [
             'id' => $playground_element_id,
             'error' => 'Не найден'
         ]);
     }
-} else {
-    $result = mysqli_query($link, "SELECT * FROM `playground_elements`");
+
+    goto output;
+}
+
+if (isset($input_playground_id)) {
+    $result = mysqli_query($link, "SELECT * FROM `playground_elements` WHERE `playground_id` = '$input_playground_id'");
+
+    if (!$row = mysqli_fetch_assoc($result)) {
+        $content_key_result = 'error';
+        $content_result = 'Не найдено';
+        $content_status = 'Not Found';
+
+        goto output;
+    }
 
     while ($row = mysqli_fetch_assoc($result)) {
         $playground_element_id = $row['id'];
-        $playground_id = $row['playground_id'];
         $element_id = $row['element_id'];
 
-        array_push($content, [
+        array_push($content_result, [
             'id' => $playground_element_id,
-            'playground_id' => $playground_id,
             'element_id' => $element_id
         ]);
     }
+
+    goto output;
 }
+
+$result = mysqli_query($link, "SELECT * FROM `playground_elements`");
+
+while ($row = mysqli_fetch_assoc($result)) {
+    $playground_element_id = $row['id'];
+    $playground_id = $row['playground_id'];
+    $element_id = $row['element_id'];
+
+    array_push($content_result, [
+        'id' => $playground_element_id,
+        'playground_id' => $playground_id,
+        'element_id' => $element_id
+    ]);
+}
+
+output:
+
+array_push($content, [
+    $content_key_result => $content_result,
+    'status' => $content_status
+]);
 
 if ($content) {
     $json_str = json_encode($content, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK);
