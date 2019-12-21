e7805a57e821daa47d39c894c3af327e76515c2d
Ilya Sidorchik <ilya@sidorchik.ru>
2019-12-21 15:22:03 +0300

    Update districts

--- api/v1/districts/index.php
+++ api/v1/districts/index.php
@@ -3,11 +3,19 @@
 header('Content-type: application/json');
 
 $ini = parse_ini_file('../../../app.ini', true);
-
-$link = mysqli_connect($ini[database][host], $ini[database][user], $ini[database][password], $ini[database][name]) or die('Ошибка');
-mysqli_set_charset($link, 'utf8');
+$link = mysqli_connect($ini[database][host], $ini[database][user], $ini[database][password], $ini[database][name]);
 
 $content = [];
+$successMessage = false;
+$resultField = 'error';
+$resultMessage = [];
+
+if (mysqli_connect_errno()) {
+    $resultMessage = 'Соединение с базой данных не удалось';
+    goto output;
+}
+
+mysqli_set_charset($link, 'utf8');
 
 $district_id = htmlspecialchars($_GET['id']);
 
@@ -17,31 +25,40 @@ if ($district_id != '') {
     $name = $row['name'];
 
     if (isset($name)) {
-        array_push($content, [
+        $successMessage = true;
+        $resultField = 'result';
+        $resultMessage = [
             'id' => $district_id,
             'name' => $name
-        ]);
+        ];
     } else {
-        array_push($content, [
-            'id' => $district_id,
-            'error' => 'Не найден'
-        ]);
+        $resultMessage = "Не найден id, равный $district_id";
     }
 } else {
     $result = mysqli_query($link, "SELECT * FROM `districts`");
+
+    $successMessage = true;
+    $resultField = 'result';
 
     while ($row = mysqli_fetch_assoc($result)) {
         $district_id = $row['id'];
         $name = $row['name'];
 
-        array_push($content, [
+        array_push($resultMessage, [
             'id' => $district_id,
             'name' => $name
         ]);
     }
 }
 
+output:
+
+array_push($content, [
+    'success' => $successMessage,
+    $resultField => $resultMessage
+]);
+
 if ($content) {
-    $json_str = json_encode($content, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK);
+    $json_str = json_encode($content, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
     echo $json_str;
 }
