0c72d6c16e3d9ab2485e888569b12c8db4820032
Ilya Sidorchik <ilya@sidorchik.ru>
2019-12-21 13:34:53 +0300

    Update areas

--- api/v1/areas/index.php
+++ api/v1/areas/index.php
@@ -3,11 +3,19 @@
 header('Content-type: application/json');
 
 $ini = parse_ini_file('../../../app.ini', true);
-
 $link = mysqli_connect($ini[database][host], $ini[database][user], $ini[database][password], $ini[database][name]) or die('Ошибка');
-mysqli_set_charset($link, 'utf8');
 
 $content = [];
+$successMessage = false;
+$resultField = 'error';
+$resultMessage = '';
+
+if (mysqli_connect_errno()) {
+    $resultMessage = 'Соединение с базой данных не удалось';
+    goto next;
+}
+
+mysqli_set_charset($link, 'utf8');
 
 $area_id = htmlspecialchars($_GET['id']);
 
@@ -17,31 +25,40 @@ if ($area_id != '') {
     $name = $row['name'];
 
     if (isset($name)) {
-        array_push($content, [
+        $successMessage = true;
+        $resultField = 'result';
+        $resultMessage = [
             'id' => $area_id,
             'name' => $name
-        ]);
+        ];
     } else {
-        array_push($content, [
-            'id' => $area_id,
-            'error' => 'Не найден'
-        ]);
+        $resultMessage = "Не найден id = $area_id";
     }
 } else {
     $result = mysqli_query($link, "SELECT * FROM `areas`");
+    $successMessage = true;
+    $resultField = 'result';
+    $resultMessage = [];
 
     while ($row = mysqli_fetch_assoc($result)) {
         $area_id = $row['id'];
         $name = $row['name'];
 
-        array_push($content, [
+        array_push($resultMessage, [
             'id' => $area_id,
             'name' => $name
         ]);
     }
 }
 
+next:
+
+array_push($content, [
+    'success' => $successMessage,
+    $resultField => $resultMessage
+]);
+
 if ($content) {
-    $json_str = json_encode($content, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK);
+    $json_str = json_encode($content, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
     echo $json_str;
 }
