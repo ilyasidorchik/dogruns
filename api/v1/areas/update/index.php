9209fd22816145f41713a3eab86623eee64ededb
Ilya Sidorchik <ilya@sidorchik.ru>
2019-12-21 13:56:50 +0300

    Update areas/update

--- api/v1/areas/update/index.php
+++ api/v1/areas/update/index.php
@@ -1,22 +1,33 @@
 <?php
 
-$ini = parse_ini_file('../../../../app.ini', true);
+header('Content-type: application/json');
 
+$ini = parse_ini_file('../../../../app.ini', true);
 $link = mysqli_connect($ini[database][host], $ini[database][user], $ini[database][password], $ini[database][name]) or die('Ошибка');
-mysqli_set_charset($link, 'utf8');
 
 $content = [];
+$successMessage = false;
+$resultField = 'error';
+$resultMessage = '';
+
+if (mysqli_connect_errno()) {
+    $resultMessage = 'Соединение с базой данных не удалось';
+    goto next;
+}
+
+mysqli_set_charset($link, 'utf8');
 
 $area_id = htmlspecialchars($_GET['id']);
 $inputJSON = file_get_contents('php://input');
 $input = json_decode($inputJSON, true);
 $new_name = $input[0]['name'];
 
+// Validation
+$new_name = str_replace("'", "", $new_name);
+
 $api_key = htmlspecialchars($_GET['API_KEY']);
 
 if (isset($area_id)) {
-    header('Content-type: application/json');
-
     if ($api_key == $ini[api][key]) {
         $result = mysqli_query($link, "SELECT `name` FROM `areas` WHERE `id` = '$area_id'");
         $row = mysqli_fetch_assoc($result);
@@ -30,40 +41,37 @@ if (isset($area_id)) {
             if (empty($other_id)) {
                 mysqli_query($link, "UPDATE `areas` SET `name` = '$new_name' WHERE `id` = '$area_id'");
 
-                array_push($content, [
-                    'success' => true,
+                $successMessage = true;
+                $resultField = 'result';
+                $resultMessage = [
                     'id' => $area_id,
-                    'result' => [
-                        'old' => [
-                            'name' => $old_name
-                        ],
-                        'new' => [
-                            'name' => $new_name
-                        ]
+                    'old' => [
+                        'name' => $old_name
+                    ],
+                    'new' => [
+                        'name' => $new_name
                     ]
-                ]);
+                ];
             } else {
-                array_push($content, [
-                    'success' => false,
-                    'error' => "$new_name уже есть в таблице"
-                ]);
+                $resultMessage = "$new_name уже есть в таблице";
             }
         } else {
-            array_push($content, [
-                'success' => false,
-                'error' => "Отсутствует id, равный $area_id"
-            ]);
+            $resultMessage = "Отсутствует id, равный $area_id";
         }
     } else {
-        $errorMessage = ($api_key != '') ? 'Неверный ключ' : 'Не хватает ключа';
-        $errorMessage .= '. Обратитесь к администратору: ilya@sidorchik.ru';
-
-        array_push($content, [
-            'success' => false,
-            'error' => $errorMessage
-        ]);
+        $resultMessage = ($api_key != '') ? 'Неверный ключ' : 'Не хватает ключа';
+        $resultMessage .= '. Обратитесь к администратору: ilya@sidorchik.ru';
     }
+}
 
-    $json_str = json_encode($content, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK);
+next:
+
+array_push($content, [
+    'success' => $successMessage,
+    $resultField => $resultMessage
+]);
+
+if ($content) {
+    $json_str = json_encode($content, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
     echo $json_str;
 }
